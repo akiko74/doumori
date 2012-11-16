b07824a2b34dab2a5642b614b0116ae687c37fd5
Akiko Saito <akiko74@yahoo.co.jp>
2012-11-16 12:25:18 +0900

    Dot picture application for Doubutsu no mori (DS game)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doumori::Application.config.secret_token = '7a550c84514caa1016ba01e890bbe356d74f63abaa8c53b9addff973d24f5129c8ff1cac3db07dfa2b67bc8b20b9e763bbe1330b278dbd65d922d01bf80cb15e'
